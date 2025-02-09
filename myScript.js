e1b24df0bdccf58db8516a7baedd8742009f5e53
Michael Glahn <112340258+mwglahn@users.noreply.github.com>
2025-02-09 15:48:08 -0700

    menu alert

---
+++ myScript.js
@@ -0,0 +1,9 @@
+// Function for Moblie Menu bar
+function clickMenu() {
+    var x = document.getElementsById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
