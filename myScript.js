6af94c11a110a423ce119359f987de6937c3a707
Michael Glahn <112340258+mwglahn@users.noreply.github.com>
2025-02-09 16:07:37 -0700

    no way it was a space

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@
 function clickMenu() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += "responsive";
+        x.className += " responsive";
     } else {
         x.className = "topnav";
     }
