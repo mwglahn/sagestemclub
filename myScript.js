63f0f35c464f3d6631e5cacd0e76d9a1f508d55b
Michael Glahn <112340258+mwglahn@users.noreply.github.com>
2025-02-09 21:14:13 -0700

    jean yuhs

--- myScript.js
+++ myScript.js
@@ -7,21 +7,3 @@ function clickMenu() {
         x.className = "topnav";
     }
 }
-
-// Function for sticky nav bar because stupid css position:sticky; doesnt work
-window.onscroll = function() {stickyNav()};
-
-// Get the navbar
-var navbar = document.getElementById("myTopnav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function stickyNav() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
